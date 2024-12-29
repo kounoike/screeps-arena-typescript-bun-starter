43ae97d00a984c26f3b2bc7953ac9965ee976757
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2024-12-29 14:26:06 +0000

    change directory layout

--- src/ctf/basic/storategy-main.ts
+++ src/ctf/basic/storategy-main.ts
@@ -2,8 +2,8 @@ import { Flag } from 'arena/season_beta/capture_the_flag/basic/prototypes'
 import { Creep } from 'game/prototypes'
 import { getObjectsByPrototype } from 'game/utils'
 import { getLogger } from 'loglevel'
-import { initializeLog } from '../common/log.ts'
-import { myAwesomeLogic } from '../common/sample.ts'
+import { initializeLog } from '../../common/log.ts'
+import { myAwesomeLogic } from '../../common/sample.ts'
 import { moveStoratagy } from './storategy/move.ts'
 
 initializeLog()
