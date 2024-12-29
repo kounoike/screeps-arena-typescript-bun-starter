faca223be7cab9f2fd3d2028cd7bf4b418c5182b
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2024-12-29 14:30:44 +0000

    use logger

--- src/ctf/basic/sample-main.ts
+++ src/ctf/basic/sample-main.ts
@@ -1,14 +1,19 @@
 import { Flag } from 'arena/season_beta/capture_the_flag/basic/prototypes'
 import { Creep } from 'game/prototypes'
 import { getObjectsByPrototype } from 'game/utils'
+import { getLogger } from 'loglevel'
+
+const logger = getLogger('main')
 
 export function loop() {
   const enemyFlag = getObjectsByPrototype(Flag).find(object => !object.my)
   if (!enemyFlag) {
+    logger.error('enemy flag not found')
     return
   }
   const myCreeps = getObjectsByPrototype(Creep).filter(object => object.my)
   for (const creep of myCreeps) {
+    logger.info(`creep:${creep.id} moveTo flag:${enemyFlag.id}`)
     creep.moveTo(enemyFlag)
   }
 }
